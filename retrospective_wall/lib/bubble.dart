1cbbdde5130c8127bd1867da015b643d20558230
supertommc <correiodotomas@gmail.com>
2020-11-03 16:09:21 +0000

    Add Bubble form, lacks styling

---
+++ retrospective_wall/lib/bubble.dart
@@ -0,0 +1,13 @@
+
+
+class Bubble {
+  String text;
+  bool isAnonymous;
+  int category;
+
+  Bubble(String text, bool isAnonymous, int category){
+    this.text = text;
+    this.isAnonymous = isAnonymous;
+    this.category = category;
+  }
+}
--- retrospective_wall/lib/main.dart
+++ retrospective_wall/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:retrospective_wall/bubble.dart';
 
 void main() {
   runApp(MyApp());
@@ -50,21 +52,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _navigateAndDisplayBubble(BuildContext context) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context)=> BubbleScreen()),
+    );
+
+    Bubble b = result;
+
+    print(b.text);
+    print(b.isAnonymous);
+    print(b.category);
+
   }
 
   @override
   Widget build(BuildContext context) {
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -77,41 +82,117 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              _navigateAndDisplayBubble(context);
+            },
+            child: Text('New Bubble'),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
+
+class BubbleScreen extends StatefulWidget{
+  @override
+  _BubbleScreen createState() => _BubbleScreen();
+}
+
+class _BubbleScreen extends State<BubbleScreen>{
+
+  TextEditingController controller = new TextEditingController();
+
+  int _value = 1;
+
+  bool isAnonymous = false;
+
+  Bubble createBubble(String text, bool isAnonymous, int value){
+    return new Bubble(text, isAnonymous, value);
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Bubble"),
+      ),
+      body: Column(
+        children: [
+          Text("New Review:"),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.cyan,
+              border: Border.all(),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton(
+                  value: _value,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("Wishes"),
+                      value: 1,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Risks"),
+                      value: 2,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Appreciations"),
+                      value: 3,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("Puzzles"),
+                      value: 4,
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState((){
+                      _value = value;
+                    });
+                  }
+              ),
+            ),
+          ),
+          CheckboxListTile(
+              title: Text("Anonymous Review: "),
+              value: isAnonymous,
+              onChanged: (newValue) {
+                setState(() {
+                  isAnonymous = newValue;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+          ),
+          TextField(
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 10,
+            controller: controller,
+            textAlign: TextAlign.left,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter your feedback',
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Bubble b = createBubble(controller.text, isAnonymous, _value);
+                Navigator.pop(context, b);
+              },
+              child: Text("Submit"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
