c0063153c4da9e9bf1b422b3f7def6ceadddaa8c
Joao Sousa <joaosousa.pt@gmail.com>
2020-11-19 19:28:32 +0000

    Added blue gradient as background.
    Light blue gradient in the "Categories" and "Subdivisions Detail" Pages.

--- retrospective_wall/lib/subdivision_detail.dart
+++ retrospective_wall/lib/subdivision_detail.dart
@@ -20,39 +20,48 @@ class _SubdivisionDetail extends State<SubdivisionDetail> {
         appBar: AppBar(
           title: Text('Subdivisions Detail'),
         ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              Column(children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 16.0),
-                  height: 500,
-                  child: ListView(
-                    children: bubbles
-                        .map((bubble) => GestureDetector(
-                              child: Container(
-                                child: Text(bubble.text,
-                                    style:
-                                        Theme.of(context).textTheme.bodyText2),
-                                alignment: Alignment.center,
-                                margin: new EdgeInsets.all(20.0),
-                                padding: new EdgeInsets.all(20.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  border: Border.all(
-                                    color: Colors.black,
-                                    width: 1,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blue.shade50, Colors.blue.shade600]
+            )
+          ),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Column(children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 16.0),
+                    height: 500,
+                    child: ListView(
+                      children: bubbles
+                          .map((bubble) => GestureDetector(
+                                child: Container(
+                                  child: Text(bubble.text,
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2),
+                                  alignment: Alignment.center,
+                                  margin: new EdgeInsets.all(20.0),
+                                  padding: new EdgeInsets.all(20.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade50,
+                                    border: Border.all(
+                                      color: Colors.black,
+                                      width: 1,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                              ),
-                              onTap: () => _onBubbleTap(context, bubble.id),
-                            ))
-                        .toList(),
-                  ),
-                )
-              ])
-            ],
+                                onTap: () => _onBubbleTap(context, bubble.id),
+                              ))
+                          .toList(),
+                    ),
+                  )
+                ])
+              ],
+            ),
           ),
         ));
   }
