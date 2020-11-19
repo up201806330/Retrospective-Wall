1c14350a6a68333be6031c9625e240d412240254
Joao Sousa <joaosousa.pt@gmail.com>
2020-11-19 20:29:33 +0000

    UI changes in the "Add bubble" screen

--- retrospective_wall/lib/style.dart
+++ retrospective_wall/lib/style.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const LargeTextSize = 26.0;
-const MediumTextSize = 20.0;
-const BodyTextSize = 16.0;
+const LargeTextSize = 32.0;
+const MediumTextSize = 26.0;
+const BodyTextSize = 20.0;
 
 // const String FontNameDefault = 'Montserrat';
 
