9056eb3c0dd3ec40f82096fd45a47468caaf7e44
supertommc <correiodotomas@gmail.com>
2020-12-03 22:31:11 +0000

    Started integrating likes with firebase

--- retrospective_wall/lib/feedback_stats.dart
+++ retrospective_wall/lib/feedback_stats.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
 import 'package:retrospective_wall/feedback_text.dart';
@@ -15,11 +16,14 @@ class FeedbackStats extends StatefulWidget {
 }
 
 class _FeedbackStats extends State<FeedbackStats> {
-
   final DocumentSnapshot _bubble;
 
   bool thumbsDown = false;
   bool thumbsUp = false;
+  bool initialThumbsUp = false;
+  bool initialThumbsDown = false;
+
+  bool hasChecked = false;
 
   int likeCounter;
   int dislikeCounter;
@@ -30,7 +34,6 @@ class _FeedbackStats extends State<FeedbackStats> {
     this.likeCounter = _bubble['nLikes'];
     this.dislikeCounter = _bubble['nDislikes'];
   }
-
 
   showAlertDialog(BuildContext context) {
     // set up the button
@@ -59,11 +62,47 @@ class _FeedbackStats extends State<FeedbackStats> {
     );
   }
 
-
   @override
   Widget build(BuildContext context) {
+    print(hasChecked);
+    if (!hasChecked) {
+      Future<Null> preferences = FirebaseFirestore.instance
+          .collection('UsersPreferences')
+          .where('userId', isEqualTo: FirebaseAuth.instance.currentUser.uid)
+          .get()
+          .then((value) {
+        var docs = value.docs;
+        if (docs.length > 0) {
+          var document = docs[0];
+          List likeIds = document['likeIds'];
+          for (var i = 0; i < likeIds.length; i++) {
+            if (likeIds[i] == _bubble.id) {
+              setState(() {
+                thumbsUp = true;
+                initialThumbsUp = true;
+                thumbsDown = false;
+                hasChecked = true;
+              });
+              break;
+            }
+          }
 
-    // Bubble bubble = new Bubble(_bubble.id,_bubble['title'],_bubble['isAnonymous'],_bubble['category'],FeedbackText('Summary',_bubble['text']));
+          List dislikeIds = document['dislikeIds'];
+          for (var i = 0; i < dislikeIds.length; i++) {
+            if (dislikeIds[i] == _bubble.id) {
+              setState(() {
+                thumbsDown = true;
+                initialThumbsDown = true;
+                thumbsUp = false;
+                hasChecked = true;
+              });
+              break;
+            }
+          }
+        } else
+          return null;
+      });
+    }
 
     return WillPopScope(
       onWillPop: _updateLikesDislikes,
@@ -73,9 +112,8 @@ class _FeedbackStats extends State<FeedbackStats> {
           children: [
             LikeButton(
               size: LikeButtonSize,
-              // isLiked: null,
               circleColor:
-              CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                  CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
               bubblesColor: BubblesColor(
                 dotPrimaryColor: Color(0xff33b5e5),
                 dotSecondaryColor: Color(0xff0099cc),
@@ -88,10 +126,8 @@ class _FeedbackStats extends State<FeedbackStats> {
                   size: LikeButtonSize,
                 );
               },
-
               likeCount: likeCounter,
               isLiked: thumbsUp,
-
               countBuilder: (int count, bool isLiked, String text) {
                 /*print(count); print('\n');
                     print(isLiked); print('\n');*/
@@ -114,7 +150,7 @@ class _FeedbackStats extends State<FeedbackStats> {
             LikeButton(
               size: LikeButtonSize,
               circleColor:
-              CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
+                  CircleColor(start: Color(0xff00ddff), end: Color(0xff0099cc)),
               bubblesColor: BubblesColor(
                 dotPrimaryColor: Color(0xff33b5e5),
                 dotSecondaryColor: Color(0xff0099cc),
@@ -129,9 +165,7 @@ class _FeedbackStats extends State<FeedbackStats> {
               },
               likeCount: dislikeCounter,
               isLiked: thumbsDown,
-
               likeCountPadding: const EdgeInsets.only(left: 3.0, right: 3.0),
-
               countBuilder: (int count, bool isLiked, String text) {
                 var color = isLiked ? Colors.orange : Colors.grey;
                 Widget result;
@@ -151,22 +185,13 @@ class _FeedbackStats extends State<FeedbackStats> {
             ),
           ],
         ),
-
       ),
     );
-
-
   }
 
-  Future<bool> onLikeButtonTapped(bool isLiked) async{
-    /// send your request here
-    // final bool success= await sendRequest();
-
-    /// if failed, you can do nothing
-    // return success? !isLiked:isLiked;
-
+  Future<bool> onLikeButtonTapped(bool isLiked) async {
     setState(() {
-      if(isLoggedIn) {
+      if (isLoggedIn) {
         if (isLiked) likeCounter--;
         if (!isLiked) {
           likeCounter++;
@@ -178,8 +203,7 @@ class _FeedbackStats extends State<FeedbackStats> {
 
         isLiked = !isLiked;
         thumbsUp = isLiked;
-      }
-      else {
+      } else {
         showAlertDialog(context);
       }
     });
@@ -187,10 +211,9 @@ class _FeedbackStats extends State<FeedbackStats> {
     return isLiked;
   }
 
-  Future<bool> onDislikeButtonTapped(bool isLiked) async{
-
+  Future<bool> onDislikeButtonTapped(bool isLiked) async {
     setState(() {
-      if(isLoggedIn) {
+      if (isLoggedIn) {
         if (isLiked) dislikeCounter--;
         if (!isLiked) {
           dislikeCounter++;
@@ -201,18 +224,84 @@ class _FeedbackStats extends State<FeedbackStats> {
         }
         isLiked = !isLiked;
         thumbsDown = isLiked;
+      } else {
+        showAlertDialog(context);
       }
     });
 
     return isLiked;
   }
 
+  void updateDataFirebase(bool initDown, bool initUp, bool down, bool up,
+      QueryDocumentSnapshot doc, DocumentSnapshot bubble) {
+    print('Document again!!!!!!!!!!!!!!!!!!!!!!!');
+    print(doc);
+    print(initUp);
+    print(initDown);
+    print(up);
+    print(down);
+    if (doc != null) {
+      if (initDown == false && initUp == false) {
+        if (up) {
+          List currentLikeIds = doc['likeIds'];
+          currentLikeIds.add(_bubble.id);
+          doc.reference.update({'likeIds': currentLikeIds});
+        } else if (down) {
+          List currentDislikeIds = doc['dislikeIds'];
+          currentDislikeIds.add(_bubble.id);
+          doc.reference.update({'dislikeIds': currentDislikeIds});
+        }
+      } else if (initUp && !initDown) {
+        if (down) {
+          List currentLikeIds = doc['likeIds'];
+          List currentDislikeIds = doc['dislikeIds'];
+          currentLikeIds.removeWhere((element) => element == bubble.id);
+          currentDislikeIds.add(_bubble.id);
+          doc.reference.update(
+              {'likeIds': currentLikeIds, 'dislikeIds': currentDislikeIds});
+        } else if (!down && !up) {
+          List currentLikeIds = doc['likeIds'];
+          currentLikeIds.removeWhere((element) => element == bubble.id);
+          doc.reference.update({'likeIds': currentLikeIds});
+        }
+      } else if (initDown && !initUp) {
+        if (up) {
+          List currentLikeIds = doc['likeIds'];
+          List currentDislikeIds = doc['dislikeIds'];
+          currentDislikeIds.removeWhere((element) => element == bubble.id);
+          currentLikeIds.add(_bubble.id);
+          doc.reference.update(
+              {'likeIds': currentLikeIds, 'dislikeIds': currentDislikeIds});
+        } else if (!down && !up) {
+          List currentDislikeIds = doc['dislikeIds'];
+          currentDislikeIds.removeWhere((element) => element == bubble.id);
+          doc.reference.update({'dislikeIds': currentDislikeIds});
+        }
+      }
+    }
+  }
 
   Future<bool> _updateLikesDislikes() async {
+    if (initialThumbsDown != thumbsDown || initialThumbsUp != thumbsUp) {
+      Future<Null> preferences = FirebaseFirestore.instance
+          .collection('UsersPreferences')
+          .where('userId', isEqualTo: FirebaseAuth.instance.currentUser.uid)
+          .get()
+          .then((value) {
+        var docs = value.docs;
+        print(docs.length);
+        if (docs.length > 0) {
+          var document = docs[0];
+          updateDataFirebase(initialThumbsDown, initialThumbsUp, thumbsDown,
+              thumbsUp, document, _bubble);
+        } else
+          return null;
+      });
 
-    _bubble.reference.update({'nLikes': likeCounter, 'nDislikes': dislikeCounter});
+      _bubble.reference
+          .update({'nLikes': likeCounter, 'nDislikes': dislikeCounter});
+    }
 
     return true;
   }
-
 }
